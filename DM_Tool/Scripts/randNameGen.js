897ca9000c92e76860d71e788210ca7589b4f725
tcooley91 <tcooley1@student.cvtc.edu>
2017-03-07 18:14:49 -0600

    Random Name Generator

--- DM_Tool/Scripts/randNameGen.js
+++ DM_Tool/Scripts/randNameGen.js
@@ -1,27 +1,27 @@
 var RandomNameGen = {
-    
-    init: function() {        
+
+    init: function () {
         firstName = ['Ilbert', 'Terric', 'Searl', 'Reynald', 'Ulric', 'Conan', 'Raolin', 'Bardolf', 'Theodoric', 'Tristan'];
-        
+
         lastName = ['Silverhand', 'Ironstar', 'Sunhunter', 'Wyvernsinger', 'Highbrand', 'Cliffwalker', 'Rockcleaver', 'Farrowwood', 'Deathglade', 'Frozensun'];
-        
+
         var randomNameButton = document.querySelector('#randomNameButton');
-        
+
         output = document.querySelector('#randomName');
-        
+
         randomNameButton.addEventListener('click', RandomNameGen.displayRandomName, false);
     },
-    
-    displayRandomName: function() {
+
+    displayRandomName: function () {
         var randomIndex = RandomNameGen.randomRange(1, firstName.length);
-        
+
         var randFirstName = firstName[randomIndex];
         var randLastName = lastName[randomIndex];
-        
+
         output.textContent = randFirstName + ' ' + randLastName;
     },
-    
-    randomRange: function(min, max) {
+
+    randomRange: function (min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
 };
