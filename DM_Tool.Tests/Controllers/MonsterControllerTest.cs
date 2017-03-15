7d9dc5d0aacd3ddd97f1635c90a6bfde66f65f8b
tcooley91 <tcooley1@student.cvtc.edu>
2017-03-14 19:53:06 -0500

    Unit Tests for RandomNameGen and Player/Monster Controller

---
+++ DM_Tool.Tests/Controllers/MonsterControllerTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DM_Tool.Controllers;
+using System.Web.Mvc;
+
+namespace DM_Tool.Tests.Controllers
+{
+    /// <summary>
+    /// Summary description for MonsterControllerTest
+    /// </summary>
+    [TestClass]
+    public class MonsterControllerTest
+    {
+        public MonsterControllerTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Create()
+        {
+            MonstersController monstersTest = new MonstersController();
+
+            ViewResult result = monstersTest.Create() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
