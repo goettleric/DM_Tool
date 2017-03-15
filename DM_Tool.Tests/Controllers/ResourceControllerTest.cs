b7a8f337f70116f613d5383e2b77ba16c6528104
Eric Goettl <eric.l.goettl@gmail.com>
2017-03-15 08:19:07 -0500

    Unit test for Resources Controller

---
+++ DM_Tool.Tests/Controllers/ResourceControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DM_Tool.Controllers;
+using System.Web.Mvc;
+
+namespace DM_Tool.Tests.Controllers
+{
+    [TestClass]
+    public class ResourceControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            ResourcesController playersTest = new ResourcesController();
+
+            ViewResult result = playersTest.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
